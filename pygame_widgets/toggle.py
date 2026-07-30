2613959000e3cf6bdf9e0bcf4cab43c4c783e7fb
CURROCHKA <iivgav11@gmail.com>
2026-07-30 19:04:52 +0300

    toggle: camelCase -> snake_case, improved draw method

--- pygame_widgets/toggle.py
+++ pygame_widgets/toggle.py
@@ -1,9 +1,8 @@
 import pygame
-from pygame import gfxdraw
 
 import pygame_widgets
-from pygame_widgets.widget import WidgetBase
 from pygame_widgets.mouse import Mouse, MouseState
+from pygame_widgets.widget import WidgetBase
 
 
 class Toggle(WidgetBase):
@@ -11,78 +10,72 @@ class Toggle(WidgetBase):
         super().__init__(win, x, y, width, height)
 
         self.value = kwargs.get("startOn", False)
-        self.onColour = kwargs.get("onColour", (141, 185, 244))
-        self.offColour = kwargs.get("offColour", (150, 150, 150))
-        self.handleOnColour = kwargs.get("handleOnColour", (26, 115, 232))
-        self.handleOffColour = kwargs.get("handleOffColour", (200, 200, 200))
-        self.onClick = kwargs.get("onClick", lambda *args: None)
-        self.onClickParams = kwargs.get("onClickParams", ())
+        self.on_color = kwargs.get("on_color", (141, 185, 244))
+        self.off_color = kwargs.get("off_color", (150, 150, 150))
+        self.handle_on_color = kwargs.get("handle_on_color", (26, 115, 232))
+        self.handle_off_color = kwargs.get("handle_off_color", (200, 200, 200))
+        self.on_click = kwargs.get("on_click", lambda *args: None)
+        self.on_click_params = kwargs.get("on_click_params", ())
 
-        self.handleRadius = kwargs.get("handleRadius", int(self._height / 1.3))
+        self.handle_radius = kwargs.get("handle_radius", int(self._height / 1.3))
         self.radius = self._height // 2
 
-        self.colour = self.onColour if self.value else self.offColour
-        self.handleColour = self.handleOnColour if self.value else self.handleOffColour
+        self.color = self.on_color if self.value else self.off_color
+        self.handle_color = (
+            self.handle_on_color if self.value else self.handle_off_color
+        )
 
     def toggle(self):
         self.value = not self.value
-        self.colour = self.onColour if self.value else self.offColour
-        self.handleColour = self.handleOnColour if self.value else self.handleOffColour
+        self.color = self.on_color if self.value else self.off_color
+        self.handle_color = (
+            self.handle_on_color if self.value else self.handle_off_color
+        )
 
     def listen(self, events):
         if not self._hidden and not self._disabled:
-            mouseState = Mouse.get_mouse_state()
+            mouse_state = Mouse.get_mouse_state()
             x, y = Mouse.get_mouse_pos()
 
-            if self.contains(x, y):
-                if mouseState == MouseState.CLICK:
-                    self.toggle()
-                    self.onClick(*self.onClickParams)
+            if self.contains(x, y) and mouse_state == MouseState.CLICK:
+                self.toggle()
+                self.on_click(*self.on_click_params)
 
     def draw(self):
         if not self._hidden:
             pygame.draw.rect(
-                self.win, self.colour, (self._x, self._y, self._width, self._height)
+                self.win,
+                self.color,
+                (self._x, self._y, self._width, self._height),
+                border_radius=self.radius,
             )
 
-            pygame.draw.circle(
-                self.win,
-                self.colour,
-                (self._x, self._y + self._height // 2),
-                self.radius,
-            )
-            pygame.draw.circle(
-                self.win,
-                self.colour,
-                (self._x + self._width, self._y + self._height // 2),
-                self.radius,
-            )
-
-            circle = (
+            circle_center_coord = (
                 self._x
                 + (
-                    self._width - self.handleRadius + self.radius
+                    self._width - self.handle_radius + self.radius
                     if self.value
-                    else self.handleRadius - self.radius
+                    else self.handle_radius - self.radius
                 ),
                 self._y + self._height // 2,
             )
 
-            gfxdraw.filled_circle(
-                self.win, *circle, self.handleRadius, self.handleColour
+            pygame.draw.aacircle(
+                self.win, self.handle_color, circle_center_coord, self.handle_radius
             )
-            gfxdraw.aacircle(self.win, *circle, self.handleRadius, self.handleColour)
 
-    def getValue(self) -> bool:
+    def get_value(self) -> bool:
         return self.value
 
 
 if __name__ == "__main__":
+    import sys
+
     pygame.init()
     win = pygame.display.set_mode((1000, 600))
 
     toggle = Toggle(win, 100, 100, 100, 40)
-    toggle.onClick = lambda: print(toggle.getValue())
+    toggle.on_click = lambda: print(toggle.get_value())
 
     run = True
     while run:
@@ -91,7 +84,7 @@ if __name__ == "__main__":
             if event.type == pygame.QUIT:
                 pygame.quit()
                 run = False
-                quit()
+                sys.exit()
 
         win.fill((255, 255, 255))
 
